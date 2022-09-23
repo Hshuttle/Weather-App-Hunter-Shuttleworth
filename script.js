47e2bcc401991027b514a7d2853d6602c42ede1f
Hshuttle <huntershutle@gmail.com>
2022-09-23 11:43:00 -0400

    started second fetch request--got errors on calling

--- script.js
+++ script.js
@@ -12,11 +12,30 @@ function apiCall(city) {
   fetch(apiUrl).then(function (locWeather) {
     return locWeather.json().then(function (locData) {
       console.log(locData);
+      //   pulling lat long and name into an array
       var long = locData[0].lon;
       var lat = locData[0].lat;
       var cityName = locData[0].name;
-      var coordinates = [cityName, lat, long];
+      var state = locData[0].state;
+      var coordinates = [cityName, state, lat, long];
       console.log(coordinates);
+    });
+  });
+  weatherFetch();
+}
+// Fetch call for weather using what I pulled in the coordinates variable
+function weatherFetch(lat, long) {
+  var weatherURL =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    long +
+    "&appid=" +
+    apiKey;
+
+  fetch(weatherURL).then(function (locWeather2) {
+    return locWeather2.json().then(function (locData2) {
+      console.log(locData2);
     });
   });
 }
@@ -25,6 +44,7 @@ function apiCall(city) {
 $("#searchbtn").on("click", function (event) {
   var city = $("#city").val();
   apiCall(city);
+  //   weatherFetch();
   console.log(city);
   event.preventDefault();
 });
