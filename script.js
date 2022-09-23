1be447eb05cfc7111803a6dd0364ef73c360490a
Hshuttle <huntershutle@gmail.com>
2022-09-23 11:09:34 -0400

    pulled the needed information from the fetch request

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // API key for open weather"71abd236202a60a8eea87ad972a67236"
 var apiKey = "71abd236202a60a8eea87ad972a67236";
-// API for geo locating--not sure if i picked the wrong one
-// var apiUrl =
-//   "http://api.openweathermap.org/geo/1.0/direct?q=austin&limit=4&appid=" +
-//   apiKey;
-// var city = $("#city").val();
+
 // fetch call to API
 function apiCall(city) {
+  // API for geo locating--not sure if i picked the wrong one
   var apiUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
@@ -15,6 +12,11 @@ function apiCall(city) {
   fetch(apiUrl).then(function (locWeather) {
     return locWeather.json().then(function (locData) {
       console.log(locData);
+      var long = locData[0].lon;
+      var lat = locData[0].lat;
+      var cityName = locData[0].name;
+      var coordinates = [cityName, lat, long];
+      console.log(coordinates);
     });
   });
 }
