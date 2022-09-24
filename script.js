97382e22a49e5fb25a6562b02443eda7c882aff8
Hshuttle <huntershutle@gmail.com>
2022-09-24 11:56:00 -0400

    corrected my second fetch request and created cards showing just temp for now

--- script.js
+++ script.js
@@ -17,25 +17,49 @@ function apiCall(city) {
       var lat = locData[0].lat;
       var cityName = locData[0].name;
       var state = locData[0].state;
-      var coordinates = [cityName, state, lat, long];
+      var coordinates = { cityName, state, lat, long };
       console.log(coordinates);
+      weatherFetch(coordinates);
     });
   });
-  weatherFetch();
 }
 // Fetch call for weather using what I pulled in the coordinates variable
-function weatherFetch(lat, long) {
+function weatherFetch({ cityName, state, lat, long }) {
   var weatherURL =
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
     lat +
     "&lon=" +
     long +
-    "&appid=" +
+    "&units=imperial&appid=" +
     apiKey;
 
   fetch(weatherURL).then(function (locWeather2) {
     return locWeather2.json().then(function (locData2) {
       console.log(locData2);
+      var card = $("<div>").addClass("card");
+      var cardBody = $("<div>").addClass("card-body");
+      var cardHeader = $("<div>").addClass("card-header");
+      var cardTitle = $("<h2>")
+        .addClass("card-title")
+        .text(cityName + ", " + state);
+      var tempEl = $("<h4>")
+        .addClass("card-text")
+        .text(
+          "Temperature: " +
+            Math.round(locData2.main.temp) +
+            String.fromCharCode(176) +
+            "F"
+        );
+      var iconEl = $("<img>").attr(
+        "src",
+        `http://openweathermap.org/img/wn/${locData2.weather[0].icon}@2x.png`
+      );
+      $("#resultsDisplay").append(
+        card.append(
+          cardBody.append(cardHeader.append(cardTitle.append(iconEl))),
+          tempEl
+        )
+      );
     });
   });
 }
@@ -44,7 +68,6 @@ function weatherFetch(lat, long) {
 $("#searchbtn").on("click", function (event) {
   var city = $("#city").val();
   apiCall(city);
-  //   weatherFetch();
   console.log(city);
   event.preventDefault();
 });
